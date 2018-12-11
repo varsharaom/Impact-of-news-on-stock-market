7b8d0641eec39898498d39eadac2947decccce7f
Aditya Prabhu <adityalprabhu@gmail.com>
2018-12-10 23:24:42 -0500

    cleaned code

--- cleandata.py
+++ cleandata.py
@@ -29,29 +29,24 @@ read = False
 
 titles = {}
 
-# # getting all files in single file for words count
-# for filename in glob.glob(os.path.join(path, '*')):
-#     titleList = []
-#     with open(filename, 'r') as infile, open((cleanedPath+path_leaf(filename)), 'w') as outfile:
-#         lines = infile.readlines()
-#         for line in lines:
-#             if read:
-#                 outfile.write(line[2:-2] + "\n")
-#                 titleList.append(line[2:-2])
-#             if line == "sg6\n":
-#                 read = True
-#             else:
-#                 read = False
-#         titles[path_leaf(filename)[:-4]] = titleList
-#
-# infile.close()
-# outfile.close()
-#
-# # for k,v in titles.items():
-# #     print(k, len(v))
-#
-# with open("titles.txt", 'w') as dictFile:
-#     json.dump(titles, dictFile)
-#
-#
-# print(len(titles))
+# getting all files in single file for words count
+for filename in glob.glob(os.path.join(path, '*')):
+    titleList = []
+    with open(filename, 'r') as infile, open((cleanedPath+path_leaf(filename)), 'w') as outfile:
+        lines = infile.readlines()
+        for line in lines:
+            if read:
+                outfile.write(line[2:-2] + "\n")
+                titleList.append(line[2:-2])
+            if line == "sg6\n":
+                read = True
+            else:
+                read = False
+        titles[path_leaf(filename)[:-4]] = titleList
+
+infile.close()
+outfile.close()
+
+with open("titles.txt", 'w') as dictFile:
+    json.dump(titles, dictFile)
+
